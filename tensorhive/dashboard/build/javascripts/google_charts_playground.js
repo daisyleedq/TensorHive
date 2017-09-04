a69041e0ec4cdfff563de9a6d405d47a80cee290
micmarty <michal.martyniak@linux.pl>
2017-09-04 14:54:01 +0200

    build last commit
    There was a build error when js minify was turned on.
    TODO do research, why (some middleman build --verbose)

--- tensorhive/dashboard/build/javascripts/google_charts_playground.js
+++ tensorhive/dashboard/build/javascripts/google_charts_playground.js
@@ -1,20 +1,40 @@
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
+// Code only for testing and presentation purposes
+//------------------------------------------------
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(reloadChart);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Example from google charts website
+function drawPieChart(counter){
   var data = google.visualization.arrayToDataTable([
     ['Task', 'Hours per Day'],
-    ['Work',     11],
+    ['Work',     1],
     ['Eat',      2],
-    ['Commute',  2],
+    ['Commute',  1.5],
     ['Watch TV', 2],
-    ['Sleep',    7]
+    ['Sleep',    0.7* counter]
   ]);
 
   var options = {
-    title: 'My Daily Activities',
-    pieHole: 0.4,
+    title: 'My Daily Activities'
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
   chart.draw(data, options);
+}
+
+// Just 4fun, display animated chart
+async function reloadChart() {
+  var count_up = true;
+  var counter = 0;
+
+  for(counter;counter<1000;counter++){
+    drawPieChart(counter)
+    await sleep(100);
+  }
 }
--- tensorhive/dashboard/config.rb
+++ tensorhive/dashboard/config.rb
@@ -49,7 +49,7 @@ page '/*.txt', layout: false
 # What happens on command: middleman build
 configure :build do
   activate :minify_css
-  activate :minify_javascript
+  #activate :minify_javascript
 
   activate :relative_assets
 end
